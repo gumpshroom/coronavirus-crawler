6c087b94a60d45fa726d8a73d11089b14ef1410c
ggar <dra032005@gmail.com>
2020-03-03 20:24:22 -0500

    translate from chinese

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 var http = require('http')
 var request = require('request')
-var server = http.createServer(function(req,res) {
+const translate = require('translate');
+translate.engine = 'yandex';
+translate.key = 'trnsl.1.1.20200304T005113Z.2795cde4b46f70e0.0d41d338b63244dfd4b89156cb6cf1a0876c648c   ';
+async function translateContent(obj) {
+    console.log(2)
+    return new Promise(async resolve => {
+        console.log(obj)
+
+    })
+}
+var server = http.createServer(async function(req,res) {
 
     var obj = {}
-    request("http://lab.isaaclin.cn/nCoV/api/overall", function(error, response, body) {
+    request("http://lab.isaaclin.cn/nCoV/api/overall", async function(error, response, body) {
         var content = body.split("<html>", 1)[0]
         if(content) {
-            res.writeHead(200,
-                {"Content-Type" : "application/json"});
-            console.log(content)
-            obj = JSON.parse(content).results
-            console.log(obj)
-            res.end(JSON.stringify(obj));
+            //console.log(content)
+            obj = JSON.parse(content).results[0]
+            //console.log(obj)
+            for (var x = 6; x < Object.keys(obj).length - 1; x++) {
+                obj[Object.keys(obj)[x]] = obj[Object.keys(obj)[x]].toString()
+                obj[Object.keys(obj)[x]] = await translate(obj[Object.keys(obj)[x]], {from: "zh", to: "en"}).catch(function(){console.log("bad")})
+                console.log(obj[Object.keys(obj)[x]])
+            }
+                res.writeHead(200,
+                    {"Content-Type": "application/json"});
+
+                res.end(JSON.stringify(obj));
+
+
         } else {
             res.writeHead(400,
                 {"Content-Type" : "application/json"});
