e3b0584f98829d8536ffbbe25cec0b659493392d
ggar <dra032005@gmail.com>
2020-03-03 20:50:06 -0500

    translate from chinese

--- app.js
+++ app.js
@@ -3,39 +3,44 @@ var request = require('request')
 const translate = require('translate');
 translate.engine = 'yandex';
 translate.key = 'trnsl.1.1.20200304T005113Z.2795cde4b46f70e0.0d41d338b63244dfd4b89156cb6cf1a0876c648c   ';
-async function translateContent(obj) {
-    console.log(2)
+var obj = {}
+async function doRequest() {
     return new Promise(async resolve => {
-        console.log(obj)
 
+        request("http://lab.isaaclin.cn/nCoV/api/overall", async function (error, response, body) {
+            var content = body.split("<html>", 1)[0]
+            if (content) {
+                //console.log(content)
+                obj = JSON.parse(content).results[0]
+                //console.log(obj)
+                for (var x = 6; x < Object.keys(obj).length - 1; x++) {
+                    obj[Object.keys(obj)[x]] = obj[Object.keys(obj)[x]].toString()
+                    obj[Object.keys(obj)[x]] = await translate(obj[Object.keys(obj)[x]], {
+                        from: "zh",
+                        to: "en"
+                    }).catch(function () {
+                        console.log("bad")
+                    })
+                    console.log(obj[Object.keys(obj)[x]])
+                }
+
+            }
+        })
     })
 }
-var server = http.createServer(async function(req,res) {
+(async function(){
+    await doRequest();
+    console.log("loaded!");
+    setInterval(function(){doRequest()}, 300000)
+}())
 
-    var obj = {}
-    request("http://lab.isaaclin.cn/nCoV/api/overall", async function(error, response, body) {
-        var content = body.split("<html>", 1)[0]
-        if(content) {
-            //console.log(content)
-            obj = JSON.parse(content).results[0]
-            //console.log(obj)
-            for (var x = 6; x < Object.keys(obj).length - 1; x++) {
-                obj[Object.keys(obj)[x]] = obj[Object.keys(obj)[x]].toString()
-                obj[Object.keys(obj)[x]] = await translate(obj[Object.keys(obj)[x]], {from: "zh", to: "en"}).catch(function(){console.log("bad")})
-                console.log(obj[Object.keys(obj)[x]])
-            }
-                res.writeHead(200,
-                    {"Content-Type": "application/json"});
+var server = http.createServer(async function (req, res) {
 
-                res.end(JSON.stringify(obj));
+    res.writeHead(200,
+        {"Content-Type": "application/json"});
 
-
-        } else {
-            res.writeHead(400,
-                {"Content-Type" : "application/json"});
-            res.end("fail");
-        }
-    })
+    res.end(JSON.stringify(obj));
 
 });
 server.listen(process.env.PORT || 7000);
+
