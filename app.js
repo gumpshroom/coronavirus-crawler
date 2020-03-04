7fcb641b17d41dcc92fd94fc215f157a0d342294
ggar <dra032005@gmail.com>
2020-03-03 19:34:29 -0500

    writehead 200

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var http = require('http')
 var request = require('request')
 var server = http.createServer(function(req,res) {
-    res.writeHead(200,
-        {"Content-Type" : "application/json"});
+
     var obj = {}
     request("http://lab.isaaclin.cn/nCoV/api/overall", function(error, response, body) {
         var content = body.split("<html>", 1)[0]
         if(content) {
+            res.writeHead(200,
+                {"Content-Type" : "application/json"});
             console.log(content)
             obj = JSON.parse(content).results
             console.log(obj)
             res.end(JSON.stringify(obj));
         } else {
+            res.writeHead(400,
+                {"Content-Type" : "application/json"});
             res.end("fail");
         }
     })
